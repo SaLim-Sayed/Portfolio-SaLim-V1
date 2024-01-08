04d841f2abd9139d76c482ba03f1c4c93e7d9134
Salim Sayed Salim Mohammed <75137018+SaLim-Sayed@users.noreply.github.com>
2024-01-08 09:37:06 +0200

    Update Title.tsx

--- components/Global/Title.tsx
+++ components/Global/Title.tsx
@@ -18,14 +18,14 @@ export default function Title({
   return (
     <div
       className={cn(
-        "flex text-center  w-full mt-[50px] mb-[24px] leading-[35px] flex-col  justify-center items-center",
+        "flex text-center  w-full mt-[50px] mb-[24px] leading-[25px] md:leading-[35px] flex-col  justify-center items-center",
         exSt && exSt
       )}
     >
       {title && (
         <div
           className={cn(
-            "md:text-[36px] text-[24px] font-[600] text-teal-700 ",
+            "md:text-[36px] text-[24px] font-[500] md:font-[600]  text-teal-700 ",
             exStTitle && exStTitle
           )}
         >
